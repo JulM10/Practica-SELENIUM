02181703cef808a1e7f0165b0648af296f1f2c18
JulM10 <juliomadero21@gmail.com>
2025-06-13 19:10:58 -0300

    Organizando proyecto y creacion de 2 test googleSearch y loginTest

---
+++ tests/googleSearch.js
@@ -0,0 +1,19 @@
+const { Builder, By, until } = require('selenium-webdriver');
+
+async function googleSearch() {
+  const driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get('https://www.google.com');
+    const input = await driver.findElement(By.name('q'));
+    await input.sendKeys('Selenium');
+    await input.submit();
+    await driver.wait(until.titleContains('Selenium'), 3000);
+    console.log('✅ [Google] Búsqueda de Selenium exitosa.');
+  } catch (err) {
+    console.error('❌ [Google] Falló la búsqueda:', err.message);
+  } finally {
+    await driver.quit();
+  }
+}
+
+module.exports = googleSearch;
---
+++ tests/loginTest.js
@@ -0,0 +1,20 @@
+const { Builder, By, until } = require('selenium-webdriver');
+
+async function loginTest() {
+  const driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get('https://practicetestautomation.com/practice-test-login/');
+    await driver.findElement(By.id('username')).sendKeys('student');
+    await driver.findElement(By.id('password')).sendKeys('Password123');
+    await driver.findElement(By.id('submit')).click();
+
+    await driver.wait(until.elementLocated(By.xpath("//h1[contains(text(),'Logged In Successfully')]")), 3000);
+    console.log('✅ [Login] Login exitoso');
+  } catch (err) {
+    console.error('❌ [Login] Falló el login:', err.message);
+  } finally {
+    await driver.quit();
+  }
+}
+
+module.exports = loginTest;
