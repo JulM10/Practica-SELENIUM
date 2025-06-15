43b46707270ec410de56e0faaee59645fde43031
JulM10 <juliomadero21@gmail.com>
2025-06-15 20:48:28 -0300

    se agrega test de excepcion y funcion para medir tiempo de ejecucion de test

--- index.js
+++ index.js
@@ -4,14 +4,27 @@
 
 const googleSearch = require('./tests/googleSearch');
 const loginTest = require('./tests/loginTest');
-const excepcion1Test = require('./tests/NoSuchElementException');
+const excepcion1Test = require('./tests/Exceptions/NoSuchElementException');
+const excepcion2Test = require('./tests/Exceptions/ElementNotInteractableException');
 
+// funcion para ejecutar cada test y medir su tiempo de ejecución
+async function runTest(nombre, testFn) {
+  console.log(`🔹 Iniciando ${nombre}...`);
+  const start = Date.now();
+  await testFn();
+  const end = Date.now();
+  const duracion = ((end - start) / 1000).toFixed(2);
+  console.log(`🕒 ${nombre} finalizó en ${duracion} segundos\n`);
+}
+
+// Función principal para ejecutar todos los tests
 (async () => {
   console.log('🚀 Iniciando test suite...\n');
 
-  await googleSearch();
-  await loginTest();
-  await excepcion1Test();
+  await runTest('Test Case 1: googleSearch', googleSearch);
+  await runTest('Test Case 2: Logintest', loginTest);
+  await runTest('Test Case 3: NoSuchElementException', excepcion1Test);
+  await runTest('Test Case 4: ElementNotInteractableException',excepcion2Test);
 
   console.log('\n✅ Todos los tests terminaron.');
 })();
---
+++ tests/Exceptions/ElementNotInteractableException.js
@@ -0,0 +1,29 @@
+const { Builder, By, until } = require('selenium-webdriver');
+
+// Probar excepcion NoSuchElementException test
+async function excepcion2Test() {
+  const driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get('https://practicetestautomation.com/practice-test-exceptions/');
+    await driver.findElement(By.id('add_btn')).click();
+    const row2 = await driver.wait(until.elementLocated(By.id('row2')), 5000);
+    
+    await driver.wait(until.elementIsVisible(row2), 3000);
+
+    await driver.wait(async () => {
+      const enabled = await row2.isEnabled();
+      return enabled;
+    }, 3000);
+
+    await row2.sendKeys('Texto de prueba');
+    await driver.findElement(By.name('Save')).click();
+
+    console.log('✅ [Excepción 2] Input interactuado correctamente.');
+  } catch (err) {
+    console.error('❌ [Excepción 2] Error:', err.name, '-', err.message);
+  } finally {
+    await driver.quit();
+  }
+}
+
+module.exports = excepcion2Test;
